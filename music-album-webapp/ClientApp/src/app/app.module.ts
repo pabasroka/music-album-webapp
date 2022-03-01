b62370b0d767bacde58a79b1c8057df1f375b171
Patryk <patrykyrtap@o2.pl>
2022-03-01 19:24:22 +0100

    Generate main components

--- music-album-webapp/ClientApp/src/app/app.module.ts
+++ music-album-webapp/ClientApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
