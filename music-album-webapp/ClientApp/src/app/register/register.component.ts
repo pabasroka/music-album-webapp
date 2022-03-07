70e764ec20aab0609e42829e91819c348b0938b7
Patryk <patrykyrtap@o2.pl>
2022-03-07 16:26:59 +0100

    Add register form

--- music-album-webapp/ClientApp/src/app/register/register.component.ts
+++ music-album-webapp/ClientApp/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  invalidRegister: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+      confirmPassword: '',
+      firstName: '',
+      lastName: '',
+      roleId: 2,
+      distributionId: 4,
+    });
+  }
+
+  submit(): void {
+    this.http.post("https://localhost:5001/api/users/register", this.form.getRawValue(), {
+      responseType: 'text'
+    })
+      .subscribe({
+        next: (response: string) => {
+          this.router.navigate(['/'])
+        },
+        error: (error: any) => {
+          this.invalidRegister = true;
+        }
+      });
   }
 
 }
